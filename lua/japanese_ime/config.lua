c1a57a871302776e5cee3c58721159359fa86b39
uga-rosa <uga6603@gmail.com>
2022-09-09 17:29:08 +0900

    add config

---
+++ lua/japanese_ime/config.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+M.config = {
+    keyLayout = "japanese_ime.data.azik",
+}
+
+---@param opt table
+function M.setup(opt)
+    vim.validate({
+        opt = { opt, "t" },
+    })
+
+    M.config = vim.tbl_deep_extend("force", M.config, opt)
+
+    vim.validate({
+        ["config.keyLayout"] = { M.config.keyLayout, "s" },
+    })
+end
+
+---@param name string
+---@return unknown
+function M.get(name)
+    return M.config[name] or error("Invalid option name: " .. name)
+end
+
+return M
